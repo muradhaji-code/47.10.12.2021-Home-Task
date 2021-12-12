e424c44c51ecdbd9d44dc575e4a880c4db77466e
muradhaji-code <tu201806256@code.edu.az>
2021-12-12 14:35:06 +0400

    nav buttons bug solved

--- js/script.js
+++ js/script.js
@@ -1,87 +1,23 @@
 $(document).ready(function () {
-  // let button = document.getElementById("confirmButton");
-
-  // button.addEventListener("click", function(e) {
-  //     e.preventDefault();
-
-  //     // Swal.fire({
-  //     //     title: '<strong>HTML <u>example</u></strong>',
-  //     //     icon: 'info',
-  //     //     html: 'You can use <b>bold text</b>, ' +
-  //     //         '<a href="//sweetalert2.github.io">links</a> ' +
-  //     //         'and other HTML tags',
-  //     //     showCloseButton: true,
-  //     //     showCancelButton: true,
-  //     //     focusConfirm: false,
-  //     //     confirmButtonText: '<i class="fa fa-thumbs-up"></i> Great!',
-  //     //     confirmButtonAriaLabel: 'Thumbs up, great!',
-  //     //     cancelButtonText: '<i class="fa fa-thumbs-down"></i>',
-  //     //     cancelButtonAriaLabel: 'Thumbs down'
-  //     // })
-
-  //     Swal.fire({
-  //         title: 'Do you want to save the changes?',
-  //         showDenyButton: true,
-  //         showCancelButton: true,
-  //         confirmButtonText: 'Save',
-  //         denyButtonText: `Don't save`,
-  //     }).then((result) => {
-  //         /* Read more about isConfirmed, isDenied below */
-  //         if (result.isConfirmed) {
-  //             Swal.fire('Saved!', '', 'success')
-  //         } else if (result.isDenied) {
-  //             Swal.fire('Changes are not saved', '', 'info')
-  //         }
-  //     })
-
-  //     // const swalWithBootstrapButtons = Swal.mixin({
-  //     //     customClass: {
-  //     //         confirmButton: 'btn btn-success',
-  //     //         cancelButton: 'btn btn-danger'
-  //     //     },
-  //     //     buttonsStyling: false
-  //     // })
-
-  //     // swalWithBootstrapButtons.fire({
-  //     //     title: 'Are you sure?',
-  //     //     text: "You won't be able to revert this!",
-  //     //     icon: 'warning',
-  //     //     showCancelButton: true,
-  //     //     confirmButtonText: 'Yes, delete it!',
-  //     //     cancelButtonText: 'No, cancel!',
-  //     //     reverseButtons: true
-  //     // }).then((result) => {
-  //     //     if (result.isConfirmed) {
-  //     //         swalWithBootstrapButtons.fire(
-  //     //             'Deleted!',
-  //     //             'Your file has been deleted.',
-  //     //             'success'
-  //     //         )
-  //     //     } else if (
-  //     //         /* Read more about handling dismissals below */
-  //     //         result.dismiss === Swal.DismissReason.cancel
-  //     //     ) {
-  //     //         swalWithBootstrapButtons.fire(
-  //     //             'Cancelled',
-  //     //             'Your imaginary file is safe :)',
-  //     //             'error'
-  //     //         )
-  //     //     }
-  //     // })
-  // });
-
   $(".owl-carousel").owlCarousel({
-    items: 5,
-    margin: 15,
-    // loop: true,
-    // center: true
-    // autoplay: true,
-    // autoplayTimeout: 1000,
-    // autoplayHoverPause: true,
-    // startPosition: 2,
     nav: true,
-    rewind: false,
-    dotsEach: true,
+    navText: [
+      '<i class="fas fa-chevron-left"></i>',
+      '<i class="fas fa-chevron-right"></i>',
+    ],
+    margin: 15,
     dots: false,
+    items: 1,
+    responsive: {
+      480: {
+        items: 2,
+      },
+      768: {
+        items: 4,
+      },
+      992: {
+        items: 6,
+      },
+    },
   });
 });
